a9837ebc516e29a2259e31a6eb64392425c1090a
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-17 21:57:47 -0300

    import router and libraries

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+import VueFormulate from '@braid/vue-formulate'
+import { es } from '@braid/vue-formulate-i18n'
+
+import './css/materialize.css'
+import './css/style.css'
+import 'jquery'
+import './js/materialize.js'
+Vue.use(VueFormulate,  {
+  plugins: [ es ],
+  locale: 'es'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
