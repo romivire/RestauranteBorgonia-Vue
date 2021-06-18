676aafceeb9287893a8ced2a14b6738414905cce
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-17 21:59:09 -0300

    add Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="page-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Restaurante La Borgonia</h5>
+          <p class="grey-text text-lighten-4">Nos encontramos en el kilomtro 580 de la Ruta Nacional 33, ubicado en la ciudad de Mendoza. Te esperamos!</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Contacto</h5>
+          <ul>
+            <li><a class="white-text">Email: laBorgonia@gmail.com</a></li>
+            <li><a class="white-text">Telefono: 2913584785</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Redes Sociales</h5>
+          <ul>
+            <li><a class="fab fa-twitter" href="https://twitter.com/?lang=es"></a>  Twitter</li>
+            <li><a class="fab fa-facebook" href="https://www.facebook.com/"></a>  Facebook</li>
+            <li><a class="fab fa-instagram" href="https://www.instagram.com/"></a>  Instagram</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Restaurante La Borgonia - Copyright 2021 
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.page-footer{
+    background-color: #7a1313
+}
+</style>
