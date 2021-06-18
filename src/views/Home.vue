186a864ba44f9f284d1b694fe17b3315566104dc
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-17 21:58:27 -0300

    create router and views for routes

---
+++ src/views/Home.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <div class="parallax-container parallax1 valign-wrapper">
+    <div class="section no-pad-bot backgroundShadow">
+      <div class="container">
+        <div class="row center">
+          <h3 class="header col s12 light">La Borgonia</h3>  
+          <h5 class="header col s12">Lo invitamos a disfrutar de una experiencia gastronomica</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="section-special">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Experencia en la atencion</h5>
+
+            <p class="light landing-text">Contamos con mas de 40 anos de experiencia en el rubro, lo cual nos permite lograr la mejor y mas rapida atencion para cada uno de nuestros clientes</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Enfocados en el cliente</h5>
+
+            <p class="light landing-text">Nuestro mayor objetivo es lograr que todos nuestros clientes puedan tener una experiencia gastronomica inolvidable desde el momento que ingresan al lugar. Trabajamos todos los dias para superarnos y lograr un lugar unico</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Trabajo en equipo</h5>
+
+            <p class="light landing-text">Estamos convencidos de que los mejores resultados se logran en equipo. Es por esto que contamos con un completo y experimentado equipo que se supera dia a dia para llevar los mejores platos a la mesa de cada cliente.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container parallax2 valign-wrapper">
+    <div class="section no-pad-bot backgroundShadow">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12">Un lugar para que disfrutes de la naturaleza y la gastronomia</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="section-special">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send"></i></h3>
+          <h4>Nuestra Historia</h4>
+          <p class="left-align light landing-text">En el ano 1980 en la ciudad de Mendoza, Carlos Borgnia decidio emprender el sueno que mas anhelaba: dar a conocer sus creaciones gastronomicas. Con el paso del tiempo se armo una gran familia que mantiene vivo el sueno de Carlos y ofrece a cada uno de sus clientes una gran variedad gastronomica asi como tambien un agradable espacio rodeado de naturaleza. Veni a conocernos.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container parallax3 valign-wrapper">
+    <div class="section no-pad-bot backgroundShadow">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12">Variedad gastronomica y calidad asegurada en cada uno de nuestros platos</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.section-special{
+    padding-top: 6rem;
+    padding-bottom: 6rem;
+}
+.backgroundShadow{
+    background: linear-gradient(rgba(255, 255, 255, 0.633), rgba(255, 255, 255, 0.721));
+    color:black;
+    font-weight: bolder;
+    
+}
+.landing-text{
+    font-size: 1.3rem;
+}
+.parallax1{
+     background: url('../assets/background4.jpg') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+.parallax2{
+    background: url('../assets/background5.jpg') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+.parallax3{
+    background: url('../assets/background6.jpg') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+.parallax-container{
+    
+    min-height: 500px;
+    background-size: cover;
+}
+
+.infobox {
+  float: left;
+  width: 80px;
+  height: 80px;
+  padding: 10px;
+  margin: 0 20px 20px 0;
+  border-radius: 20px;
+  border: 1px solid #ccc;
+  .title {
+    font-weight: bold;
+  }
+  .value {
+    padding: 10px 0 0 0;
+    font-size: 2rem;
+  }
+}
+</style>
---
+++ src/views/ReservaEdicion.vue
@@ -0,0 +1,170 @@
+<template>
+    <div>
+         <div class="container">
+            
+                <div class="row card-panel">
+                    <div class="col s12 l6 center card-panel">
+                            <div class="col s12">
+                                <h3  class="light">Su reserva actual</h3>    
+                            </div>
+                            <FormulateForm #default="{isValid}" @submit="handleSubmit">
+                                <!-- v-model="formValues"  -->
+                                
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        
+                                            <FormulateInput
+                                                type="text"
+                                                label="ID Reserva"
+                                                name="identificador"
+                                                validation="required|number"
+                                                placeholder="Ingrese el ID de la reserva"
+
+                                            />
+                                            <FormulateInput
+                                            type="submit"
+                                            label="Buscar reserva"
+                                            :disabled="!isValid"
+                                        />
+                                        
+                                    </div>
+                                </div>     
+                            </FormulateForm>
+                             <div class="col s12">
+                                 
+                                <h3  class="light">Datos de su reserva</h3>    
+                            </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        
+                                            <FormulateInput
+                                                type="text"
+                                                label="Fecha"
+                                                name="Fecha"
+                                                :disabled=true
+                                            />
+                                        
+                                    </div>
+                                </div>                              
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                            <FormulateInput
+                                                type="text"
+                                                label="Hora"
+                                                name="Hora"
+                                                validation="required"
+                                                :disabled=true
+                                            />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                            <FormulateInput
+                                                type="text"
+                                                name="Cantidad de personas"
+                                                label="Cantidad de personas"
+                                                :disabled=true
+                                            />
+                                        
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <FormulateInput
+                                        type="textarea"
+                                        name="Observacion"
+                                        label="Observacion"
+                                        :disabled=true
+                                        />
+                                        
+                                    </div>
+                                </div>
+                    </div>
+                    <div id="formModificar" style="visibility:hidden" class="col s12 l6 center">
+                        <div class="col s12">
+                                <h3  class="light">Modifique su reserva</h3>    
+                            </div>
+                            <FormulateForm>
+                                <!-- v-model="formValues2" -->
+                                <div class="row center">
+                                    <div class="input-field col s12">
+                                        
+                                            <FormulateInput
+                                                type="date"
+                                                label="Fecha"
+                                                name="Fecha"
+                                                validation="required|after"
+                                            />
+                                        
+                                    </div>
+                                </div>                              
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <label class="label-special">Hora</label>
+                                        <!-- v-model="value" -->
+                                            <FormulateInput
+                                            
+                                                :options="{'20:00': '20:00', '20:30': '20:30', '21:00': '21:00','21:30': '21:30'}"
+                                                type="select"
+                                                placeholder="Seleccione una hora"
+                                                name="Hora"
+                                                validation="required"
+                                            />
+                                        
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <label class="label-special">Cantidad de personas</label>
+                                            <FormulateInput
+                                                id="cantidad"
+                                                name="Cantidad de personas"
+                                                placeholder="Cantidad de personas"
+                                                validation="required|number|max:50"
+                                                
+                                            />
+                                        
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <label class="label-special">Observacion</label>
+                                        <FormulateInput
+                                        type="textarea"
+                                        name="Observacion"
+                                        placeholder="Indica tu observacion para la reserva"
+                                        help="Longitud maxima 100 caracteres"
+                                        validation="max:100,length"
+                                        error-behavior="live"
+                                        />
+                                        
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <FormulateInput
+                                            type="submit"
+                                            label="Confirmar reserva"
+                                            
+                                        />
+                                    </div>
+                                </div>
+                                
+                                
+                            </FormulateForm>
+                    </div>
+                </div>
+         </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        handleSubmit() {
+        document.getElementById('formModificar').style.visibility='visible'
+    }
+}
+}
+</script>
