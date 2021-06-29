ac1be03ec1f28d3fa1b8167c669df2601ac89899
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-29 19:24:15 -0300

    Change view Reserva Creacion and connect with API

--- src/views/ReservaCreacion.vue
+++ src/views/ReservaCreacion.vue
@@ -8,14 +8,13 @@
                              <div class="col s12">
                                 <h3  class="light">Reserva</h3>    
                             </div>
-                            <FormulateForm v-model="formValues">
+                            <FormulateForm #default="{isValid}" @submit="handleSubmit">
                                 <div class="row center">
                                     <div class="input-field col s12">
-                                        
+                                        <label class="label-special">Fecha</label>
                                             <FormulateInput
                                                 type="date"
-                                                label="Fecha"
-                                                name="Fecha"
+                                                id="fechaIngresada"
                                                 validation="required|after"
                                             />
                                         
@@ -25,11 +24,10 @@
                                     <div class="input-field col s12">
                                         <label class="label-special">Hora</label>
                                             <FormulateInput
-                                                v-model="value"
                                                 :options="{'20:00': '20:00', '20:30': '20:30', '21:00': '21:00','21:30': '21:30'}"
                                                 type="select"
                                                 placeholder="Seleccione una hora"
-                                                name="Hora"
+                                                id="horaIngresada"
                                                 validation="required"
                                             />
                                         
@@ -39,7 +37,7 @@
                                     <div class="input-field col s12">
                                         <label class="label-special">Cantidad de personas</label>
                                             <FormulateInput
-                                                name="Cantidad de personas"
+                                                id="cantidadIngresada"
                                                 placeholder="Cantidad de personas"
                                                 validation="required|number|max:50"
                                             />
@@ -51,7 +49,7 @@
                                         <label class="label-special">Observacion</label>
                                         <FormulateInput
                                         type="textarea"
-                                        name="Observacion"
+                                        id="observacionIngresada"
                                         placeholder="Indica tu observacion para la reserva"
                                         help="Longitud maxima 100 caracteres"
                                         validation="max:100,length"
@@ -63,8 +61,10 @@
                                 <div class="row">
                                     <div class="input-field col s12">
                                         <FormulateInput
+                                            id="button"
                                             type="submit"
                                             label="Confirmar reserva"
+                                            :disabled="!isValid"
                                         />
                                     </div>
                                 </div>
@@ -97,15 +97,39 @@
 </style>
 
 <script>
+import axios from "axios";
+const headers= {
+    "Access-Control-Allow-Origin" : "*"
+}
 
   export default {
     components: { },
     data() {
       return {
-        horario:'Seleccione un horario',
-        formValues: {},
-        value:{}
+        
       };
     },
-  };
+    methods:{
+        handleSubmit() {
+         let post = {
+                fecha: document.getElementById('fechaIngresada').value,
+                hora: document.getElementById('horaIngresada').value,
+                cantidad: document.getElementById('cantidadIngresada').value,
+                observacion:document.getElementById('observacionIngresada').value
+            };
+            
+            axios.post('https://romivire-servicioweb.herokuapp.com/reserva/create', post, {headers: headers})
+            .then((response) => {
+                window.alert(response.data.message+'. Su reserva es la numero: '+response.data.body.idReserva);
+                this.clearInputsIngresados();
+            });
+        },
+        clearInputsIngresados(){
+                document.getElementById('fechaIngresada').value='',
+                document.getElementById('horaIngresada').value='',
+                document.getElementById('cantidadIngresada').value='',
+                document.getElementById('observacionIngresada').value=''
+        },
+    }
+};
 </script>
